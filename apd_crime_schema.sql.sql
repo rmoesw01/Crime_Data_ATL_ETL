d5f02570f47bfddf407da9e9439baee93e83163c
Jeremy Hamley <jeremyhamley@gmail.com>
2020-08-11 21:22:28 -0400

    updates

---
+++ apd_crime_schema.sql.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE apd_crime_data (
+    crime_type varchar(30)   ,
+    report_number int PRIMARY KEY  ,
+    report_date date   ,
+    location varchar  ,
+    beat float  ,
+    neighborhood varchar   ,
+   	npu varchar(10)   ,
+    lat float   ,
+    long float  
+  
+     
+);
+
+CREATE TABLE apd_police_shooting (
+    id int  PRIMARY KEY ,
+    name varchar(30)   ,
+    date date  ,
+    manner_of_death varchar(30)  ,
+    armed varchar(30)   ,
+    age float   ,
+    gender varchar(1)  ,
+    race varchar(10)   ,
+    city varchar(30)   ,
+    state varchar(2)   ,
+    signs_of_mental_illness boolean  ,
+    threat_level varchar(15)   ,
+    flee varchar(15)   ,
+    body_cam boolean   ,
+    arms_category varchar(30)   
+
+);
